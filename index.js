34f8657b307c2bc2eb1c1a4f39cfdb35626ec135
scer <81870902+scerviez@users.noreply.github.com>
2022-03-09 00:40:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const { Telegraf, Markup } = require('telegraf');
-const save = require('instagram-save');
+const instagram = require("@phaticusthiccy/open-apis");
 const bot = new Telegraf(process.env.token);
-var path = require('path');
-const fs = require('fs-extra');
-var prefix = "/";
-var dir = './anime';
-
-if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, {recursive: true});
-}
 
 bot.start((ctx) => ctx.reply("Hai "+ctx.from.username+" Aku adalah bot untuk mendownload video/photo dari instagram"));
 bot.help((ctx) => ctx.reply('Silahkan kirim linknya'));
@@ -16,18 +8,9 @@ bot.help((ctx) => ctx.reply('Silahkan kirim linknya'));
 bot.on('text', (ctx) => {
 ctx.reply("Mendownload")
 let link = ctx.message.text;
-if (link.startsWith("https://www.instagram.com/")){save(`${link}`, 'anime/').then(res => {
-if (path.extname(`anime/${res.file}`) === ".jpg"){
-    ctx.replyWithPhoto({ source: `${res.file}`} , {caption: '@igbijabot'});
-    };
-    })
-    if (path.extname(`anime/${res.file}`) === ".mp4"){
-        ctx.replyWithVideo({ source: `${res.file}`}, {caption: '@igbijabot' });
-    };                                              
-} else {
-    ctx.reply("Link tidak valid")
-}
-
+    instagram.insta_post("https://www.instagram.com/p/Ca15RMBvwiu/?utm_source=ig_web_copy_link").then(async (data) => {
+    ctx.replyWithPhoto(data.post1.url)
 })
+
 
 bot.launch();
